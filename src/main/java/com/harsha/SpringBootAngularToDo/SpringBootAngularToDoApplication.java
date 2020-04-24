5ab69e24108c67fdf398765b9032a2a73335433e
HarshaVardhan <Harshavardhan.Athaluri@ge.com>
2020-04-24 11:50:24 +0530

    directory and architecture creation

---
+++ src/main/java/com/harsha/SpringBootAngularToDo/SpringBootAngularToDoApplication.java
@@ -0,0 +1,13 @@
+package com.harsha.SpringBootAngularToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harsha/SpringBootAngularToDo/SpringBootAngularToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsha.SpringBootAngularToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
